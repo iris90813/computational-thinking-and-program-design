c91dad5decfef8de54d535a6b5cc01fb02061fc9
iris90813 <61811416+iris90813@users.noreply.github.com>
2020-04-14 22:36:43 +0800

    week6＿名字年紀體重

---
+++ week6_a108060066_施靚玟 .py
@@ -0,0 +1,17 @@
+print("我的名字是施靚玟")
+print("我今年18歲")
+print("我的體重60公斤")
+
+name = "施靚玟"
+print("我的名字是","施靚玟")
+year = 18
+print("我今年",year,"歲")
+weight = 60
+print("我的體重",weight,"公斤")
+
+name = input("請輸入你的名字")
+print("我的名字是",name)
+year = input("請輸入你的年紀")
+print("我今年",year)
+weight = input("請輸入你的體重")
+print("我的體重是",weight)
