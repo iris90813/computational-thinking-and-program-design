1ee1c5f3d55f1f553a891d7e209953af348da010
iris90813 <61811416+iris90813@users.noreply.github.com>
2020-05-27 20:28:03 +0800

    Add files via upload

---
+++ week12.py
@@ -0,0 +1,11 @@
+#input
+麵條=input("有麵條嗎? (YES/NO)")
+醬料=input("有醬料嗎?(YES/NO)")
+
+#PROCESS
+if 麵條 == "YES":
+    if 醬料 == "YES":
+        print("今晚吃義大利麵吧")
+       
+#output
+print("well done!")          
